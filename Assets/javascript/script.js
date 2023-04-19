f0a336fa49155e753cde18937d1ad7f48942b1a9
rstreep <127055958+rstreep@users.noreply.github.com>
2023-04-18 23:28:34 -0400

    added current date to header and more time slots for work day

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -1,7 +1,21 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var today = dayjs();
+var saveButtonEl = $('.btn')
+var toDoEl = $('')
+
 $(function () {
+
+  // var today = dayjs();
+  $('#currentDay').text(today.format('dddd, MMM DD'))
+  var hour = dayjs().hour();
+  if (hour)
+  // var saveButtonEl = $('.btn')
+  // var toDoEl = $('.description')
+  saveButtonEl.on('click', function() {
+    console.log(toDoEl);
+  });
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -18,6 +32,4 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
 });
